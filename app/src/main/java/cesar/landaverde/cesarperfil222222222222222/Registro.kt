12a489e0599b8e66f255b8d0030b5722ff61107f
Cesar Landaverde <20230355@ricaldone.edu.sv>
2024-06-18 21:13:44 -0600

    casi

---
+++ app/src/main/java/cesar/landaverde/cesarperfil222222222222222/Registro.kt
@@ -0,0 +1,31 @@
+package cesar.landaverde.cesarperfil222222222222222
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registro)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+       val txtcorreoRegis = findViewById<EditText>(R.id.txtCorreoRegistro)
+        val txtcontraRegis = findViewById<EditText>(R.id.TxtcontraseñaRegistro)
+        val btnRegistrar = findViewById<Button>(R.id.btnRegistar)
+
+        btnRegistrar.setOnClickListener
+
+
+
+    }
+
+}
